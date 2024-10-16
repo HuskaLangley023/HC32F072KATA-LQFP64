9c982ef01ad9856af6310c09f588f5dc8d2e82a0
HuskaLangley023 <zhu_yixiao@sjtu.edu.cn>
2024-10-16 22:16:23 +0800

    template finished

---
+++ init/cmake-build-debug-mingw-stm32/CMakeFiles/init.elf.dir/DependInfo.cmake
@@ -0,0 +1,66 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "D:/HC32F072KATA-LQFP64/draft/init/Core/Startup/startup_stm32f427iihx.s" "D:/HC32F072KATA-LQFP64/draft/init/cmake-build-debug-mingw-stm32/CMakeFiles/init.elf.dir/Core/Startup/startup_stm32f427iihx.s.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "DEBUG"
+  "STM32F427xx"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "D:/HC32F072KATA-LQFP64/draft/init/Core/Inc"
+  "D:/HC32F072KATA-LQFP64/draft/init/Drivers/STM32F4xx_HAL_Driver/Inc"
+  "D:/HC32F072KATA-LQFP64/draft/init/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy"
+  "D:/HC32F072KATA-LQFP64/draft/init/Drivers/CMSIS/Device/ST/STM32F4xx/Include"
+  "D:/HC32F072KATA-LQFP64/draft/init/Drivers/CMSIS/Include"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "D:/HC32F072KATA-LQFP64/draft/init/Core/Src/gpio.c" "CMakeFiles/init.elf.dir/Core/Src/gpio.c.obj" "gcc" "CMakeFiles/init.elf.dir/Core/Src/gpio.c.obj.d"
+  "D:/HC32F072KATA-LQFP64/draft/init/Core/Src/main.c" "CMakeFiles/init.elf.dir/Core/Src/main.c.obj" "gcc" "CMakeFiles/init.elf.dir/Core/Src/main.c.obj.d"
+  "D:/HC32F072KATA-LQFP64/draft/init/Core/Src/stm32f4xx_hal_msp.c" "CMakeFiles/init.elf.dir/Core/Src/stm32f4xx_hal_msp.c.obj" "gcc" "CMakeFiles/init.elf.dir/Core/Src/stm32f4xx_hal_msp.c.obj.d"
+  "D:/HC32F072KATA-LQFP64/draft/init/Core/Src/stm32f4xx_it.c" "CMakeFiles/init.elf.dir/Core/Src/stm32f4xx_it.c.obj" "gcc" "CMakeFiles/init.elf.dir/Core/Src/stm32f4xx_it.c.obj.d"
+  "D:/HC32F072KATA-LQFP64/draft/init/Core/Src/syscalls.c" "CMakeFiles/init.elf.dir/Core/Src/syscalls.c.obj" "gcc" "CMakeFiles/init.elf.dir/Core/Src/syscalls.c.obj.d"
+  "D:/HC32F072KATA-LQFP64/draft/init/Core/Src/sysmem.c" "CMakeFiles/init.elf.dir/Core/Src/sysmem.c.obj" "gcc" "CMakeFiles/init.elf.dir/Core/Src/sysmem.c.obj.d"
+  "D:/HC32F072KATA-LQFP64/draft/init/Core/Src/system_stm32f4xx.c" "CMakeFiles/init.elf.dir/Core/Src/system_stm32f4xx.c.obj" "gcc" "CMakeFiles/init.elf.dir/Core/Src/system_stm32f4xx.c.obj.d"
+  "D:/HC32F072KATA-LQFP64/draft/init/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal.c" "CMakeFiles/init.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal.c.obj" "gcc" "CMakeFiles/init.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal.c.obj.d"
+  "D:/HC32F072KATA-LQFP64/draft/init/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.c" "CMakeFiles/init.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.c.obj" "gcc" "CMakeFiles/init.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.c.obj.d"
+  "D:/HC32F072KATA-LQFP64/draft/init/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.c" "CMakeFiles/init.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.c.obj" "gcc" "CMakeFiles/init.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.c.obj.d"
+  "D:/HC32F072KATA-LQFP64/draft/init/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma_ex.c" "CMakeFiles/init.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma_ex.c.obj" "gcc" "CMakeFiles/init.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma_ex.c.obj.d"
+  "D:/HC32F072KATA-LQFP64/draft/init/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_exti.c" "CMakeFiles/init.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_exti.c.obj" "gcc" "CMakeFiles/init.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_exti.c.obj.d"
+  "D:/HC32F072KATA-LQFP64/draft/init/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash.c" "CMakeFiles/init.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash.c.obj" "gcc" "CMakeFiles/init.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash.c.obj.d"
+  "D:/HC32F072KATA-LQFP64/draft/init/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ex.c" "CMakeFiles/init.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ex.c.obj" "gcc" "CMakeFiles/init.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ex.c.obj.d"
+  "D:/HC32F072KATA-LQFP64/draft/init/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ramfunc.c" "CMakeFiles/init.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ramfunc.c.obj" "gcc" "CMakeFiles/init.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ramfunc.c.obj.d"
+  "D:/HC32F072KATA-LQFP64/draft/init/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_gpio.c" "CMakeFiles/init.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_gpio.c.obj" "gcc" "CMakeFiles/init.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_gpio.c.obj.d"
+  "D:/HC32F072KATA-LQFP64/draft/init/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr.c" "CMakeFiles/init.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr.c.obj" "gcc" "CMakeFiles/init.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr.c.obj.d"
+  "D:/HC32F072KATA-LQFP64/draft/init/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.c" "CMakeFiles/init.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.c.obj" "gcc" "CMakeFiles/init.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.c.obj.d"
+  "D:/HC32F072KATA-LQFP64/draft/init/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc.c" "CMakeFiles/init.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc.c.obj" "gcc" "CMakeFiles/init.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc.c.obj.d"
+  "D:/HC32F072KATA-LQFP64/draft/init/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc_ex.c" "CMakeFiles/init.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc_ex.c.obj" "gcc" "CMakeFiles/init.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc_ex.c.obj.d"
+  "D:/HC32F072KATA-LQFP64/draft/init/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim.c" "CMakeFiles/init.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim.c.obj" "gcc" "CMakeFiles/init.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim.c.obj.d"
+  "D:/HC32F072KATA-LQFP64/draft/init/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim_ex.c" "CMakeFiles/init.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim_ex.c.obj" "gcc" "CMakeFiles/init.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim_ex.c.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ init/cmake-build-debug-mingw-stm32/CMakeFiles/init.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.c.obj.d
@@ -0,0 +1,35 @@
+CMakeFiles/init.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.c.obj: \
+ D:\HC32F072KATA-LQFP64\draft\init\Drivers\STM32F4xx_HAL_Driver\Src\stm32f4xx_hal_cortex.c \
+ D:\HC32F072KATA-LQFP64\draft\init\Drivers\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal.h \
+ D:\HC32F072KATA-LQFP64\draft\init\Core\Inc/stm32f4xx_hal_conf.h \
+ D:\HC32F072KATA-LQFP64\draft\init\Drivers\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_rcc.h \
+ D:\HC32F072KATA-LQFP64\draft\init\Drivers\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_def.h \
+ D:\HC32F072KATA-LQFP64\draft\init\Drivers\CMSIS\Device\ST\STM32F4xx\Include/stm32f4xx.h \
+ D:\HC32F072KATA-LQFP64\draft\init\Drivers\CMSIS\Device\ST\STM32F4xx\Include/stm32f427xx.h \
+ D:\HC32F072KATA-LQFP64\draft\init\Drivers\CMSIS\Include/core_cm4.h \
+ C:\ST\STM32CubeCLT_1.16.0\GNU-tools-for-STM32\bin\../lib/gcc/arm-none-eabi/12.3.1/include/stdint.h \
+ C:\ST\STM32CubeCLT_1.16.0\GNU-tools-for-STM32\bin\../lib/gcc/arm-none-eabi/12.3.1/../../../../arm-none-eabi/include/stdint.h \
+ C:\ST\STM32CubeCLT_1.16.0\GNU-tools-for-STM32\bin\../lib/gcc/arm-none-eabi/12.3.1/../../../../arm-none-eabi/include/machine/_default_types.h \
+ C:\ST\STM32CubeCLT_1.16.0\GNU-tools-for-STM32\bin\../lib/gcc/arm-none-eabi/12.3.1/../../../../arm-none-eabi/include/sys/features.h \
+ C:\ST\STM32CubeCLT_1.16.0\GNU-tools-for-STM32\bin\../lib/gcc/arm-none-eabi/12.3.1/../../../../arm-none-eabi/include/_newlib_version.h \
+ C:\ST\STM32CubeCLT_1.16.0\GNU-tools-for-STM32\bin\../lib/gcc/arm-none-eabi/12.3.1/../../../../arm-none-eabi/include/sys/_intsup.h \
+ C:\ST\STM32CubeCLT_1.16.0\GNU-tools-for-STM32\bin\../lib/gcc/arm-none-eabi/12.3.1/../../../../arm-none-eabi/include/sys/_stdint.h \
+ D:\HC32F072KATA-LQFP64\draft\init\Drivers\CMSIS\Include/cmsis_version.h \
+ D:\HC32F072KATA-LQFP64\draft\init\Drivers\CMSIS\Include/cmsis_compiler.h \
+ D:\HC32F072KATA-LQFP64\draft\init\Drivers\CMSIS\Include/cmsis_gcc.h \
+ D:\HC32F072KATA-LQFP64\draft\init\Drivers\CMSIS\Include/mpu_armv7.h \
+ D:\HC32F072KATA-LQFP64\draft\init\Drivers\CMSIS\Device\ST\STM32F4xx\Include/system_stm32f4xx.h \
+ D:\HC32F072KATA-LQFP64\draft\init\Drivers\STM32F4xx_HAL_Driver\Inc/Legacy/stm32_hal_legacy.h \
+ C:\ST\STM32CubeCLT_1.16.0\GNU-tools-for-STM32\bin\../lib/gcc/arm-none-eabi/12.3.1/include/stddef.h \
+ D:\HC32F072KATA-LQFP64\draft\init\Drivers\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_rcc_ex.h \
+ D:\HC32F072KATA-LQFP64\draft\init\Drivers\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_gpio.h \
+ D:\HC32F072KATA-LQFP64\draft\init\Drivers\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_gpio_ex.h \
+ D:\HC32F072KATA-LQFP64\draft\init\Drivers\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_exti.h \
+ D:\HC32F072KATA-LQFP64\draft\init\Drivers\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_dma.h \
+ D:\HC32F072KATA-LQFP64\draft\init\Drivers\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_dma_ex.h \
+ D:\HC32F072KATA-LQFP64\draft\init\Drivers\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_cortex.h \
+ D:\HC32F072KATA-LQFP64\draft\init\Drivers\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_flash.h \
+ D:\HC32F072KATA-LQFP64\draft\init\Drivers\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_flash_ex.h \
+ D:\HC32F072KATA-LQFP64\draft\init\Drivers\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_flash_ramfunc.h \
+ D:\HC32F072KATA-LQFP64\draft\init\Drivers\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_pwr.h \
+ D:\HC32F072KATA-LQFP64\draft\init\Drivers\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_pwr_ex.h
