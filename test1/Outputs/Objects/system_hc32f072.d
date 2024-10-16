9e59c928957eec9b36dc402b6501451bf4a8d4af
HuskaLangley023 <zhu_yixiao@sjtu.edu.cn>
2024-10-16 22:26:52 +0800

    open a new project

---
+++ test1/Outputs/Objects/system_hc32f072.d
@@ -0,0 +1,15 @@
+./outputs/objects/system_hc32f072.o: \
+  hc32f072\mcu\common\system_hc32f072.c hc32f072\mcu\common\base_types.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdio.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\string.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stddef.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\assert.h \
+  hc32f072\mcu\common\hc32f072.h \
+  C:\Users\zhu_y\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\core_cm0plus.h \
+  C:\Users\zhu_y\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_version.h \
+  C:\Users\zhu_y\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_compiler.h \
+  C:\Users\zhu_y\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_armclang.h \
+  hc32f072\mcu\common\system_hc32f072.h hc32f072\driver\inc\sysctrl.h \
+  hc32f072\driver\inc\ddl.h hc32f072\mcu\common\board_evbhc32lf07x.h \
+  hc32f072\mcu\common\interrupts_hc32f072.h userapp\inc\ddl_device.h
